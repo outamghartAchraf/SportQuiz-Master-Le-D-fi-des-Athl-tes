b4bcfa8695d8ae200d698ffb222055e5e3eae6cb
outamghartAchraf <outamghart2000@gmail.com>
2026-03-17 12:52:01 +0000

    Create basic ResultPage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import QuizPage from "./pages/QuizPage";
-import ResultPage from "./pages/ResultPage";
+import ResultPage from "./pages/ResultPages";
 import ErrorPage from "./pages/ErrorPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
