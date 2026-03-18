27a58deb6eb1ed1a3426628ddea349d970881ae4
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-03-18 14:07:57 +0000

    vérification du fetch et gestion des erreurs

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,4 +3,8 @@ export const fetchQuestions = async (amount = 10, difficulty = "") => {
   let url = `https://opentdb.com/api.php?amount=${amount}&category=21`;
   if (difficulty) url += `&difficulty=${difficulty}`;
   const res = await fetch(url);
+  const data = await res.json();
+  if (data.response_code !== 0) throw new Error("No questions available");
+
+
 }
